d487c4d71486b862d1bdbe6f1a7f561c66f70223
Lavanya <bitweft@gmail.com>
2017-05-25 12:37:21 +0530

    Add support for connecting to WAP WIFI network

--- app/src/main/java/com/vuclip/viu/settingsmanager/WifiSettingsManagerActivity.java
+++ app/src/main/java/com/vuclip/viu/settingsmanager/WifiSettingsManagerActivity.java
@@ -1,6 +1,7 @@
 package com.vuclip.viu.settingsmanager;
 
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,23 +9,61 @@ import android.util.Log;
 
 import com.vuclip.viu.settingsmanager.constants.IntentExtras;
 
+import java.util.List;
+
 public class WifiSettingsManagerActivity extends AppCompatActivity {
+    Bundle extras;
+    String tag = WifiSettingsManagerActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toggleWifi();
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            changeSettings();
+        }
+    }
+
+    private void changeSettings() {
+        if (extras.containsKey(IntentExtras.ENABLE_WIFI)) {
+            toggleWifi();
+        }
+        if (extras.containsKey(IntentExtras.NETWORK_SSID) && extras.containsKey(IntentExtras.NETWORK_PASSWORD)) {
+            connectToNetwork();
+        }
     }
 
     private void toggleWifi() {
-        Bundle extras = getIntent().getExtras();
-        if (extras != null && extras.containsKey(IntentExtras.ENABLE_WIFI)) {
-            boolean enableWifi = Boolean.parseBoolean(extras.getString(IntentExtras.ENABLE_WIFI));
-            Log.d(WifiSettingsManagerActivity.class.getSimpleName(), "enableWifi = " + enableWifi);
+        boolean enableWifi = Boolean.parseBoolean(extras.getString(IntentExtras.ENABLE_WIFI));
+        Log.d(tag, "enableWifi = " + enableWifi);
 
-            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            wifiManager.setWifiEnabled(enableWifi);
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(enableWifi);
+    }
+
+    private void connectToNetwork() {
+        String networkSSID = extras.getString(IntentExtras.NETWORK_SSID);
+        String networkPassword = extras.getString(IntentExtras.NETWORK_PASSWORD);
+
+        WifiConfiguration wifiConfiguration = new WifiConfiguration();
+        wifiConfiguration.SSID = String.format("\"%s\"", networkSSID);
+        wifiConfiguration.preSharedKey = String.format("\"%s\"", networkPassword);
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.addNetwork(wifiConfiguration);
+
+        if (wifiManager.isWifiEnabled()) {
+            List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
+            for (WifiConfiguration configuredNetwork : configuredNetworks) {
+                if (configuredNetwork.SSID != null && configuredNetwork.SSID.equals(String.format("\"%s\"", networkSSID))) {
+                    Log.d(tag, "Connecting to network with networkSSID = " + networkSSID);
+                    wifiManager.disconnect();
+                    wifiManager.enableNetwork(configuredNetwork.networkId, true);
+                    wifiManager.reconnect();
+                    break;
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/vuclip/viu/settingsmanager/constants/IntentExtras.java
+++ app/src/main/java/com/vuclip/viu/settingsmanager/constants/IntentExtras.java
@@ -2,4 +2,6 @@ package com.vuclip.viu.settingsmanager.constants;
 
 public class IntentExtras {
     public static final String ENABLE_WIFI = "enableWifi";
+    public static final String NETWORK_SSID = "networkSSID";
+    public static final String NETWORK_PASSWORD = "networkPassword";
 }
